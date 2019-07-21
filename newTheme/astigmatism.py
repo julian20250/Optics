8f55d853e663d6d529f930bea8888b6723c0d36a
julian <juojimenezca@unal.edu.co>
2019-07-21 13:03:38 -0500

    Uso de Poppy

---
+++ newTheme/astigmatism.py
@@ -0,0 +1,27 @@
+import poppy
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('dark_background')
+
+#Constants
+lamb=632.8e-9
+k=2*np.pi/lamb
+d_1 = 45e-2
+f=50e-3
+a=f/10
+
+coefficients_sequence = [0, 0, 0, 0, 35e-9]
+osys = poppy.OpticalSystem()
+osys.add_pupil(poppy.CircularAperture(radius=a*100)) #Radius in meters
+thinlens = poppy.ZernikeWFE(radius=a*100, coefficients=coefficients_sequence)
+osys.add_pupil(thinlens)
+osys.add_detector(pixelscale=0.010, fov_arcsec=5.0)
+psf = osys.calc_psf(lamb) #Wavelenght in meters
+poppy.display_psf(psf, title="", colorbar=False, cmap="gist_gray")
+
+plt.axis('off')
+plt.gcf().set_facecolor("black")
+plt.xlim(-2e2*a, 2e2*a)
+plt.ylim(-2e2*a, 2e2*a)
+plt.savefig("astigmatism_a=%1.2f.png"%a,  bbox_inches='tight', pad_inches = 0)
---
+++ newTheme/pupilPoppy.py
@@ -0,0 +1,24 @@
+import poppy
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('dark_background')
+
+#Constants
+lamb=632.8e-9
+k=2*np.pi/lamb
+d_1 = 45e-2
+f=50e-3
+a=f/10
+
+osys = poppy.OpticalSystem()
+osys.add_pupil(poppy.CircularAperture(radius=a*100)) #Radius in meters
+osys.add_detector(pixelscale=0.010, fov_arcsec=5.0)
+psf = osys.calc_psf(lamb) #Wavelenght in meters
+poppy.display_psf(psf, title="", colorbar=False, cmap="gist_gray")
+
+plt.axis('off')
+plt.gcf().set_facecolor("black")
+plt.xlim(-2e2*a, 2e2*a)
+plt.ylim(-2e2*a, 2e2*a)
+plt.savefig("pupil_a=%1.2f.png"%a,  bbox_inches='tight', pad_inches = 0)
